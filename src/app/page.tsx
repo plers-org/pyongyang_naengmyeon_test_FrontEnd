1d72645f4d5df81bd816687e4bcf202ddbce2e02
honi31 <winner020531@naver.com>
2026-07-29 20:25:57 +0900

    style : #14 Intro 화면 UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import { DefaultButton } from "@/components/common/DefaultButton";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Intro
+    <main className="flex min-h-screen flex-col items-center px-5 pt-31.5 pb-40">
+      <h1 className="text-center text-headline1 text-warm-gray-90">
+        나는
+        <br />
+        어떤 평양냉면 타입?
+      </h1>
+      <p className="mt-3 text-center text-subtitle1 text-warm-gray-60">
+        6가지 질문으로
+        <br />
+        나에게 딱 맞는 평양냉면 스타일을 찾아봐요!
+      </p>
+      <Image
+        src="/characters/intro-characters.png"
+        alt=""
+        width={240}
+        height={240}
+        className="mt-7.5"
+      />
+      <DefaultButton className="mt-6">내 평냉 취향 찾기</DefaultButton>
+      <p className="mt-5 text-center text-caption4 text-warm-gray-60">
+        약 2분 소요
+      </p>
     </main>
   );
 }
