472ff6031ce9bb411297096d5d02ff3a04340a6d
honi31 <winner020531@naver.com>
2026-08-01 19:37:53 +0900

    style : #18 인트로 페이지 UI 구현

--- src/app/quiz/branch/page.tsx
+++ src/app/quiz/branch/page.tsx
@@ -1,4 +1,31 @@
+import { AppBar } from "@/components/common/AppBar";
+import { DefaultButton } from "@/components/common/DefaultButton";
+import { OptionButton } from "@/components/common/OptionButton";
+
 export default function Page() {
-  // TODO: 입문자/경험자 여부 분기 화면
-  return <div>입문자/경험자 여부</div>;
+  return (
+    <main className="flex flex-col min-h-screen pt-11 pb-15">
+      <AppBar />
+      <div className="flex flex-col px-5 pt-[54px] ">
+        <p className="text-caption2 text-orange-80">Q0</p>
+        <h1 className="text-headline2 text-warm-gray-90 mt-3">
+          평양냉면을
+          <br />
+          먹어본 적 있으신가요?
+        </h1>
+        <p className="text-subtitle1 text-warm-gray-50 mt-3">
+          경험에 따라 딱 맞는 분석을 해드려요 :)
+        </p>
+        <OptionButton className="mt-[31px]" variant="option1">
+          네, 먹어봤어요
+        </OptionButton>
+        <OptionButton className="mt-3" variant="option2">
+          아니요, 처음이에요
+        </OptionButton>
+      </div>
+      <DefaultButton variant="primary" className="mt-auto mx-5">
+        다음
+      </DefaultButton>
+    </main>
+  );
 }
