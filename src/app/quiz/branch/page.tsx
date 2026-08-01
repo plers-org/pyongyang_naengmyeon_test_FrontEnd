8bb6037aef4377449a815aec731a36e60cc44e93
honi31 <winner020531@naver.com>
2026-08-01 20:20:22 +0900

    feat : #18 뒤로가기 라우터 구현

--- src/app/quiz/branch/page.tsx
+++ src/app/quiz/branch/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
 
   return (
     <main className="flex flex-col min-h-screen pt-11 pb-15">
-      <AppBar />
+      <AppBar onBack={() => router.back()} />
       <div className="flex flex-col px-5 pt-[54px]">
         <p className="text-caption2 text-orange-80">Q0</p>
         <h1 className="text-headline2 text-warm-gray-90 mt-3">
--- src/components/common/AppBar.tsx
+++ src/components/common/AppBar.tsx
@@ -1,9 +1,17 @@
 import { ChevronLeft } from "../icons/ChevronLeft";
 
-export function AppBar() {
+export type AppBarProps = {
+  onBack?: () => void;
+};
+
+export function AppBar({ onBack }: AppBarProps) {
   return (
     <div className="flex w-[375px] items-center bg-white">
-      <button className="flex h-[35.996px] pr-2 pl-3 justify-center items-center">
+      <button
+        type="button"
+        onClick={onBack}
+        className="flex h-[35.996px] pr-2 pl-3 justify-center items-center"
+      >
         <ChevronLeft />
       </button>
     </div>
